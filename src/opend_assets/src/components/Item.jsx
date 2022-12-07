1d25e6e1cca69ef22362392f3eeaecb6d646e640
dangoodie <dan.goooden.dev@gmail.com>
2022-12-07 23:08:32 +1100

    Nft's can now transfer ownership and be listed for sale

--- src/opend_assets/src/components/Item.jsx
+++ src/opend_assets/src/components/Item.jsx
@@ -3,20 +3,28 @@ import logo from "../../assets/logo.png";
 import { Actor, HttpAgent } from "@dfinity/agent";
 import { idlFactory } from "../../../declarations/nft";
 import { Principal } from "@dfinity/principal";
+import Button from "./Button";
+import { opend } from "../../../declarations/opend";
 
 function Item(props) {
-
   const [name, setName] = useState();
   const [owner, setOwner] = useState();
   const [image, setImage] = useState();
+  const [button, setButton] = useState();
+  const [priceInput, setPriceInput] = useState();
 
   const id = props.id;
 
   const localHost = "http://localhost:8080/";
-  const agent = new HttpAgent({host: localHost});
+  const agent = new HttpAgent({ host: localHost });
+
+  //TODO: When deploying live, remove this line
+  agent.fetchRootKey();
+
+  let NFTActor;
 
   async function loadNFT() {
-    const NFTActor = await Actor.createActor(idlFactory, {
+    NFTActor = await Actor.createActor(idlFactory, {
       agent,
       canisterId: id,
     });
@@ -29,15 +37,44 @@ function Item(props) {
 
     const imageData = await NFTActor.getAsset();
     const imageContent = new Uint8Array(imageData);
-    const image = URL.createObjectURL(new Blob([imageContent.buffer], {type: "image/png"}))
+    const image = URL.createObjectURL(
+      new Blob([imageContent.buffer], { type: "image/png" })
+    );
     setImage(image);
 
-
+    setButton(<Button handleClick={handleSell} text={"Sell"} />);
   }
 
   useEffect(() => {
     loadNFT();
   }, []);
+
+  let price;
+  function handleSell() {
+    console.log("Sell clicked!");
+    setPriceInput(
+      <input
+        placeholder="Price in DANG"
+        type="number"
+        className="price-input"
+        value={price}
+        onChange={(e) => (price = e.target.value)}
+      />
+    );
+    setButton(<Button handleClick={sellItem} text={"Confirm"} />)
+  }
+
+  async function sellItem() {
+    console.log("Sell item!")
+    const listingResult = await opend.listItem(props.id, Number(price));
+    console.log("listing " + listingResult);
+
+    if (listingResult == "Success") {
+      const openDId = await opend.getOpenDCanisterID();
+      const transferResult = await NFTActor.transferOwnership(openDId, true);
+      console.log("transfer: " + transferResult);
+    }
+  }
 
   return (
     <div className="disGrid-item">
@@ -48,11 +85,14 @@ function Item(props) {
         />
         <div className="disCardContent-root">
           <h2 className="disTypography-root makeStyles-bodyText-24 disTypography-h5 disTypography-gutterBottom">
-            {name}<span className="purple-text"></span>
+            {name}
+            <span className="purple-text"></span>
           </h2>
           <p className="disTypography-root makeStyles-bodyText-24 disTypography-body2 disTypography-colorTextSecondary">
             Owner: {owner}
           </p>
+          {priceInput}
+          {button}
         </div>
       </div>
     </div>
